39de62b30c68443321891def1bb971e58ec9e753
Yifan Hong <elsk@google.com>
2017-05-09 15:37:44 -0700

    Add device compatibility matrix
    Test: m system_manifest.xml -j (checks compatibility against
        framewrok manifest)
    Bug: 37954458
    Change-Id: I3d3531035cefcc60b5d9093666c032f3cf8e5687

--- device.mk
+++ device.mk
@@ -182,7 +182,8 @@ PRODUCT_PACKAGES += \
  
 # HIDL
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml \
+    $(LOCAL_PATH)/compatibility_matrix.xml:system/vendor/compatibility_matrix.xml
 
 # Keystore
 PRODUCT_PACKAGES += \

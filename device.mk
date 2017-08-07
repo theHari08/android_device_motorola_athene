baa2c1c1d90bff96fcc66fd5862c2057393812d4
Mahantesh Sanakall <msanak@codeaurora.org>
2017-08-07 16:42:06 +0530

    athene: Use build variables to build device matrices.
    Move away from building device manifest through PRODUCT_COPY_FILES.
    Use the build variables - DEVICE_MANIFEST_FILE and DEVICE_MATRIX_FILE
    to build device manifest and device compatibility files repectively.
    
    Change-Id: Ieb3e410118ee9fe00c6b03e32a983e584960d8dc

--- device.mk
+++ device.mk
@@ -180,11 +180,6 @@ PRODUCT_COPY_FILES += \
 # IMS
 PRODUCT_PACKAGES += \
     libshims_ims
- 
-# HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml \
-    $(LOCAL_PATH)/compatibility_matrix.xml:system/vendor/compatibility_matrix.xml
 
 # Keystore
 PRODUCT_PACKAGES += \

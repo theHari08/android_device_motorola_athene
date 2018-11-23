c4116499d88d44364ec5b33d992028ab3b09a8ca
Rajat Gupta <rjtgupta84@gmail.com>
2018-11-23 12:00:38 +0530

    Build moto sensor service

--- device.mk
+++ device.mk
@@ -357,6 +357,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service_moto \
     libsensorhub \
     motosh \
     sensorhub.msm8952 \
